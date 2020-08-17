5c1db046992c07e9cc3dfc251b9d26eaa5a5b36d
Aia Rupsom <aiale88@gmail.com>
2020-08-17 12:03:03 +0300

    add head routes to index

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,3 +1,4 @@
 exports.UsersRouter = require('./users.routes');
 exports.ListingsRouter = require('./listings.routes');
 exports.FarmsRouter = require('./farms.routes');
+exports.HeadsRouter = require('./heads.routes');
